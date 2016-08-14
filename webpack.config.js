74f61519dcb36ea623e38f026b40efa444eeb159
Jason Goemaat <jason.goemaat@gmail.com>
2016-08-14 05:27:45 -0500

    Works, need css-loader option 'css?-url' to disable handling urls for fonts

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
 var path = require('path');
 
 module.exports = {
@@ -15,16 +17,26 @@ module.exports = {
                         query: {
                             name: '[name].css'
                         }
-                    }, 'extract', 'css', 'sass' // compile with sass, then css, then extract to file
+                    }, 'extract', 'css?-url', 'sass' // compile with sass, then css, then extract to file
                 ] 
             }
         ]
     },
     resolve: {
-        extensions: ['', '.css', '.sass', '.scss', '.js']
+        extensions: ['', '.css', '.sass', '.scss', '.js',
+            // bootstrap fonts
+            //'.eot'
+        ]
     },
     plugins: [
         new HtmlWebpackPlugin({ template: './src/index.html' }),
+        new CopyWebpackPlugin([ { from: 'public' },
+            {
+                from: 'node_modules/bootstrap-sass/assets/fonts/bootstrap/*',
+                to: 'assets/fonts',
+                flatten: true
+            }
+        ]),
     ],
 
     // sass config
